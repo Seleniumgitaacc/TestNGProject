16e2bde37573c6540825a221ffc7b0dbc7874f46
Vrushali More <vrushalimore990@gmail.com>
2023-05-20 12:58:49 +0530

    Update framwork and loginPage and Login TestPage

---
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase {
+
+	  
+	   
+		//Page Factory - OR:
+		@FindBy(name="Username")
+		WebElement Username;
+	    
+		//Page Factory - OR:
+		@FindBy(name="Password")
+		WebElement Password;
+
+		@FindBy(xpath="//input[@type='submit']")
+		WebElement loginBtn;
+		
+		//Page Factory - OR:
+		@FindBy(xpath="//a[contains(text(),'Sign Up')]")
+		WebElement signUpBtn;
+		
+		@FindBy(xpath="(//img[@class='img-responsive'])[1]")
+		WebElement crmLogo;
+		
+		//Initiallizing the page objects
+		
+		public LoginPage(){
+			PageFactory.initElements(driver, this);
+		}
+		
+		public String validateLoginPageTitle() {
+			return driver.getTitle();
+		}
+		
+		public boolean validateCRMImage(){
+			return crmLogo.isDisplayed();
+		}
+		
+		public HomePage login(String un, String pwd){
+			Username.sendKeys(un);
+			Password.sendKeys(pwd);
+			loginBtn.click();
+			    //	JavascriptExecutor js = (JavascriptExecutor)driver;
+			    //	js.executeScript("arguments[0].click();", loginBtn);
+			    	
+			return new HomePage();
+		}
+}
